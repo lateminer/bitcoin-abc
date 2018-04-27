6e4469de1c63d906e0e0eb0ccf60bf1f67379449
Amaury Séchet <deadalnix@gmail.com>
2018-04-27 17:25:31 +0200

    Update devtool notes to reference the correct copyright notice.
    Summary: As per title.
    
    Test Plan:
      \o/
    
    Reviewers: #bitcoin_abc, schancel
    
    Reviewed By: #bitcoin_abc, schancel
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D1315

--- src/util.cpp
+++ src/util.cpp
@@ -636,15 +636,10 @@ int GetNumCores() {
 std::string CopyrightHolders(const std::string &strPrefix) {
     std::string strCopyrightHolders =
         strPrefix +
-        strprintf(_(COPYRIGHT_HOLDERS), _(COPYRIGHT_HOLDERS_SUBSTITUTION));
+        strprintf(_(COPYRIGHT_HOLDERS), _(COPYRIGHT_HOLDERS_SUBSTITUTION)) +
+        "\n" + strPrefix + "The Bitcoin ABC developers" +
+        "\n" + strPrefix + "The Bitcoin Core developers" ;
 
-    // Check for untranslated substitution to make sure Bitcoin Core copyright
-    // is not removed by accident.
-    if (strprintf(COPYRIGHT_HOLDERS, COPYRIGHT_HOLDERS_SUBSTITUTION)
-            .find("Bitcoin Core") == std::string::npos) {
-        strCopyrightHolders += "\n" + strPrefix + "The Bitcoin ABC developers" +
-                               "\n" + strPrefix + "The Bitcoin Core developers" ;
-    }
     return strCopyrightHolders;
 }
 
