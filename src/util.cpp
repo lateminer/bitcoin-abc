b2d1d23abcc19c9157b5b739cfd7daf249942dc7
lateminer <late-miner@ya.ru>
2018-07-22 21:43:01 +0300

    lore -> blackmore

--- src/util.cpp
+++ src/util.cpp
@@ -90,8 +90,8 @@
 // Application startup time (used for uptime calculation)
 const int64_t nStartupTime = GetTime();
 
-const char *const BITCOIN_CONF_FILENAME = "lore.conf";
-const char *const BITCOIN_PID_FILENAME = "lored.pid";
+const char *const BITCOIN_CONF_FILENAME = "blackmore.conf";
+const char *const BITCOIN_PID_FILENAME = "blackmored.pid";
 
 ArgsManager gArgs;
 bool fPrintToConsole = false;
@@ -484,7 +484,7 @@ static std::string FormatException(const std::exception *pex,
     char pszModule[MAX_PATH] = "";
     GetModuleFileNameA(nullptr, pszModule, sizeof(pszModule));
 #else
-    const char *pszModule = "lore";
+    const char *pszModule = "blackmore";
 #endif
     if (pex)
         return strprintf("EXCEPTION: %s       \n%s       \n%s in %s       \n",
@@ -502,13 +502,13 @@ void PrintExceptionContinue(const std::exception *pex, const char *pszThread) {
 }
 
 fs::path GetDefaultDataDir() {
-// Windows < Vista: C:\Documents and Settings\Username\Application Data\Lore
-// Windows >= Vista: C:\Users\Username\AppData\Roaming\Lore
-// Mac: ~/Library/Application Support/Lore
-// Unix: ~/.lore
+// Windows < Vista: C:\Documents and Settings\Username\Application Data\Blackmore
+// Windows >= Vista: C:\Users\Username\AppData\Roaming\Blackmore
+// Mac: ~/Library/Application Support/Blackmore
+// Unix: ~/.blackmore
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Lore";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Blackmore";
 #else
     fs::path pathRet;
     char *pszHome = getenv("HOME");
@@ -518,10 +518,10 @@ fs::path GetDefaultDataDir() {
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Lore";
+    return pathRet / "Library/Application Support/Blackmore";
 #else
     // Unix
-    return pathRet / ".lore";
+    return pathRet / ".blackmore";
 #endif
 #endif
 }
